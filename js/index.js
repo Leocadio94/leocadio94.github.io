7a166b4af9ff1a52d2e29391ff3609a66a7da487
Gabriel Leocadio Almeida da Silva <gabriel.silva73@fatec.sp.gov.br>
2016-04-14 19:48:00 -0300

    Versão inicial

---
+++ js/index.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    $("#home").backstretch("img/home.jpg");
+    $("#about").backstretch("img/about.jpg");
+    $("#portfolio").backstretch("img/portfolio.jpg");
+    $("#contact").backstretch("img/contact.jpg");
+
+    $("#toTop").hide();
+
+    // fade in & out
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 600) {
+            $('#toTop').fadeIn();
+        } else {
+            $('#toTop').fadeOut();
+        }
+    });
+    $("a[href*='#']").each(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+            location.hostname == this.hostname &&
+            this.hash.replace(/#/, '')) {
+            var $targetId = $(this.hash),
+                $targetAnchor = $('[name=' + this.hash.slice(1) + ']');
+            var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
+            if ($target) {
+                var targetOffset = $target.offset().top;
+                $(this).click(function() {
+                    $('html, body').animate({
+                        scrollTop: targetOffset
+                    }, 600);
+                    return false;
+                });
+            }
+        }
+    });
+});
